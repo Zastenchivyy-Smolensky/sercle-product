faae8f0fdb291ef256883bc6137ff16fbff9415e
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-04-25 22:28:55 +0900

    Ajax 終わり

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    favorite = current_user.favorites.build(product_id: @product.id)
+    favorite = current_user.favorites.new(product_id: @product.id)
     favorite.save
   end
 
