9715c8998f24d03caca2f0c1f473379d4a615b28
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-04 23:49:46 +0900

    編集機能途中

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include "portfolio-land"
     end
   end
+
   describe "#product-index" do
     it "正常にレスポンスを返すことできる" do
       get products_path
@@ -15,6 +16,7 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include "プロダクト一覧"
     end
   end
+
   describe "#user-product" do
     it "正常にレスポンスを返すこと" do
       get users_path
@@ -22,6 +24,7 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include "プログラマー一覧"
     end
   end
+
   describe "ログインページ" do
     it "ログインページにレスポンスを返すことができる" do
       get new_user_session_path
@@ -29,7 +32,8 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include "ログイン"
     end
   end
-  describe "新規ページ" do
+
+  describe "新規ページにアクセステスト" do
     it "新規ページに遷移できるのか?" do
       get new_user_registration_path
       expect(response).to have_http_status :ok
@@ -62,7 +66,7 @@ RSpec.describe "Products", type: :request do
       context "有効である場合" do
         let!(:product_params) {{product:{title: product.title, content:product.content, tech: product.tech, span: product.span, 
           image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.jpeg')),github: product.github, link: product.link,commitment: product.commitment}}}
-        it "投稿数" do
+        it "投稿されているかどうか?" do
           sign_in(user)
           expect{
             post products_path, params:product_params
@@ -72,7 +76,62 @@ RSpec.describe "Products", type: :request do
           post products_path, params:product_params
           expect(flash).to be_any
         end
+        # it "投稿に成功したらリダイレクトされるか?" do
+        #   post products_path, params: product_params
+        #   pending 'この先はなぜかテストが失敗するのであとで直す'
+        #   product = Product.last
+        #   expect(response).to redirect_to product
+        # end
 
+      end
+    end
+
+    describe "編集ページのアクセステスト" do
+      let!(:product) {FactoryBot.create(:product)}
+      let!(:user) {FactoryBot.create(:user)}
+      it "編集ページのタイトルが描画されていること" do
+        sign_in(user)
+        get edit_product_path(product)
+      end
+      context "無効なデータの場合" do
+        it "更新できない" do
+          patch product_path(product), params:{product:{title: "", content: product.content, tech: "product",span: 1, image: "", github:"", link:"", commitment:"" }}
+          product.reload
+          expect(product.title).to_not eq ""
+          expect(product.content).to_not eq ""
+          expect(product.tech).to_not eq ""
+          expect(product.span).to_not eq "1"
+          expect(product.image).to_not eq ""
+          expect(product.github).to_not eq ""
+          expect(product.link).to_not eq ""
+          expect(product.commitment).to_not eq ""
+        end
+        it "更新が失敗した時にeditページが表示されていること" do
+          sign_in(user)
+          get edit_product_path(product)
+          patch product_path(product), params:{product:{title: "", content: product.content, tech: "product",span: 1, image: "", github:"", link:"", commitment:"" }}
+          # pending "ここは保留"
+          # expect(response.body).to include "プロダクト編集"
+        end
+        
+      end
+    end
+    describe "削除機能テスト" do
+      let!(:user) {FactoryBot.create(:user)}
+      let!(:product) {FactoryBot.create(:product)}
+      let(:other_user) {FactoryBot.create(:archer)}
+      context "ログイン状態ではないとき" do
+        it "削除できないこと" do
+          expect{
+            delete product_path(product)
+          }.to_not change(Product, :count)
+        end
+        it "ログインページにリダイレクトされるること" do
+          delete product_path(product)
+          expect(response).to redirect_to new_user_session_path
+        end
+        it "投稿したユーザではない場合" do
+        end
       end
     end
   end
