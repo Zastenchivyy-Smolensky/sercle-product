43c81f1dc8efe1d45595c9fbff9071b71e2f8b53
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-07 12:32:42 +0900

    ..

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,10 @@ module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
+  
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
