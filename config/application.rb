a501ad2d851cdc5ae0ccd02ab7e8c9e332308e15
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-07 16:26:30 +0900

    ..

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module Portfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
     config.time_zone = 'Tokyo'
     config.generators do |g|
       g.test_framework :rspec,
