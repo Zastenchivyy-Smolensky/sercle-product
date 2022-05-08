e048cc1316ebb45863132737f45a4b840674ab5d
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-08 18:43:43 +0900

    フォロー機能テスト完了

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -1,21 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Relationships", type: :system do
+RSpec.describe 'Relationships', type: :system do
     before do
         @user1 = FactoryBot.create(:user)
         @user2 = FactoryBot.create(:user)
     end
-    describe "#create, #destroy" do
-        it "ユーザーフォロー,フォロー解除できる" do
+    describe '#create, #destroy' do
+        it 'ユーザーフォロー,フォロー解除できる' do
             sign_in(@user1)
             visit users_path(@user1)
-            click_on "フォローする"
-            expect(@user2.followings.count).to eq(0)
+            click_on 'フォローする'
+            expect(page).to have_button 'フォロー済み'
+            expect(@user1.followings.count).to eq(1)
+            expect(@user2.followers.count).to eq(1)
+            click_on 'フォロー済み'
+            expect(page).to have_button 'フォローする'
             expect(@user1.followers.count).to eq(0)
-            click_on "フォロー済み"
             expect(@user2.followings.count).to eq(0)
-            expect(@user1.followers.count).to eq(0)
-            
         end
     end
 end
