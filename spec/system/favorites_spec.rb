d44bdc8b16043a162fc2e8d866092f57d736421b
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-08 21:56:24 +0900

    いいねボタンデザイン変えた

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -1,1 +1,16 @@
+require 'rails_helper'
+RSpec.describe 'Favorites', type: :system do
+    before do
+        @user = FactoryBot.create(:user)
+        @product = FactoryBot.create(:product)
+    end
 
+    describe '#create, #describe', js: true do
+       context "いいねをクリックしたとき" do
+           it "いいねできる" do
+            sign_in(@user)
+            visit products_path
+           end
+       end
+    end
+end
